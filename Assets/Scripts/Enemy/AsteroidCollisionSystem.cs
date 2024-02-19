1ac9cc7c582c3ba802fa1cba4b4be936192fb272
Magnus Lonngi <ethanlonngi@gmail.com>
2024-02-18 18:42:03 -0600

    Make the collisions systems work with jobs

--- Assets/Scripts/Enemy/AsteroidCollisionSystem.cs
+++ Assets/Scripts/Enemy/AsteroidCollisionSystem.cs
@@ -7,52 +7,55 @@ public partial struct AsteroidCollisionSystem : ISystem {
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        EntityCommandBuffer ecb = new(Unity.Collections.Allocator.Temp);
+        EntityCommandBuffer ecb = new(Unity.Collections.Allocator.TempJob);
 
         foreach (var (playerSprite, playerTransform, playerEntity) in
             SystemAPI.Query<RefRO<Texture>, RefRO<LocalTransform>>()
             .WithAny<PlayerMovement>().WithEntityAccess()) {
-
-            foreach (var (asteroidSprite, asteroidTransform, asteroid) in
-                SystemAPI.Query<RefRO<Texture>, RefRO<LocalTransform>,
-                RefRO<Asteroid>>()) {
-
-                float3 distanceBetween =
-                    math.distancesq(playerTransform.ValueRO.Position,
-                    asteroidTransform.ValueRO.Position);
-
-                float playerHalfWidth =
-                    playerSprite.ValueRO.spriteWidth /
-                    playerSprite.ValueRO.pixelsPerUnit /
-                    playerSprite.ValueRO.spriteDivision;
-
-                float playerHalfHeight = playerSprite.ValueRO.spriteHeigth /
-                    playerSprite.ValueRO.pixelsPerUnit /
-                    playerSprite.ValueRO.spriteDivision;
-
-                float asteroidHalfWidth = asteroidSprite.ValueRO.spriteWidth /
-                    asteroidSprite.ValueRO.pixelsPerUnit /
-                    asteroidSprite.ValueRO.spriteDivision *
-                    asteroid.ValueRO.sizeMultiplier;
-
-                float asteroidHalfHeight = asteroidSprite.ValueRO.spriteHeigth /
-                    asteroidSprite.ValueRO.pixelsPerUnit /
-                    asteroidSprite.ValueRO.spriteDivision *
-                    asteroid.ValueRO.sizeMultiplier;
-
-                float distanceX = distanceBetween.x -
-                    playerHalfWidth - asteroidHalfWidth;
-
-                float distanceY = distanceBetween.y -
-                    playerHalfHeight - asteroidHalfHeight;
-
-
-                if (distanceX <= 0 || distanceY <= 0)
-                    ecb.DestroyEntity(playerEntity);
-            }
+            
+            new AsteroidCollisionJob() {
+                ecb = ecb,
+                playerTransform = playerTransform.ValueRO,
+                playerSprite = playerSprite.ValueRO,
+                playerEntity = playerEntity
+            }.Schedule(state.Dependency).Complete();
         }
 
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+    }
+
+    public partial struct AsteroidCollisionJob : IJobEntity {
+        public EntityCommandBuffer ecb;
+        public LocalTransform playerTransform;
+        public Texture playerSprite;
+        public Entity playerEntity;
+
+        void Execute(in Texture asteroidSprite, in LocalTransform asteroidTransform,
+            in Asteroid asteroid) {
+            
+            float3 distanceBetween = math.distancesq(playerTransform.Position, 
+                asteroidTransform.Position);
+
+            float playerHalfWidth = playerSprite.spriteWidth / playerSprite.pixelsPerUnit /
+                playerSprite.spriteDivision;
+
+            float playerHalfHeight = playerSprite.spriteHeigth / playerSprite.pixelsPerUnit /
+                playerSprite.spriteDivision;
+
+            float asteroidHalfWidth = asteroidSprite.spriteWidth / asteroidSprite.pixelsPerUnit / 
+                asteroidSprite.spriteDivision * asteroid.sizeMultiplier;
+
+            float asteroidHalfHeight = asteroidSprite.spriteHeigth / asteroidSprite.pixelsPerUnit / 
+                asteroidSprite.spriteDivision * asteroid.sizeMultiplier;
+
+            float distanceX = distanceBetween.x - playerHalfWidth - asteroidHalfWidth;
+
+            float distanceY = distanceBetween.y - playerHalfHeight - asteroidHalfHeight;
+
+
+            if (distanceX <= 0 || distanceY <= 0)
+                ecb.DestroyEntity(playerEntity);
+        }
     }
 }
--- Assets/Scripts/Player/BulletCollisionSystem.cs
+++ Assets/Scripts/Player/BulletCollisionSystem.cs
@@ -7,55 +7,59 @@ public partial struct BulletCollisionSystem : ISystem {
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        EntityCommandBuffer ecb = new(Unity.Collections.Allocator.Temp);
+        EntityCommandBuffer ecb = new(Unity.Collections.Allocator.TempJob);
 
-        foreach (var (asteroidSprite, asteroidTransform, asteroid,
-            asteroidEntity) in
+        foreach (var (asteroidSprite, asteroidTransform, asteroid, asteroidEntity) in
             SystemAPI.Query<RefRO<Texture>, RefRO<LocalTransform>,
             RefRO<Asteroid>>().WithEntityAccess()) {
 
-            foreach (var (bulletSprite, bulletTransform, bulletEntity)
-                in SystemAPI.Query<RefRO<Texture>, RefRO<LocalTransform>>()
-                .WithAny<Bullet>().WithEntityAccess()) {
-
-                float3 distanceBetween =
-                    math.distancesq(bulletTransform.ValueRO.Position,
-                    asteroidTransform.ValueRO.Position);
-
-                float bulletHalfWidth =
-                    bulletSprite.ValueRO.spriteWidth /
-                    bulletSprite.ValueRO.pixelsPerUnit /
-                    bulletSprite.ValueRO.spriteDivision;
-
-                float bulletHalfHeigth = bulletSprite.ValueRO.spriteHeigth /
-                    bulletSprite.ValueRO.pixelsPerUnit /
-                    bulletSprite.ValueRO.spriteDivision;
-
-                float asteroidHalfWidth = asteroidSprite.ValueRO.spriteWidth /
-                    asteroidSprite.ValueRO.pixelsPerUnit /
-                    asteroidSprite.ValueRO.spriteDivision *
-                    asteroid.ValueRO.sizeMultiplier;
-
-                float asteroidHalfHeight = asteroidSprite.ValueRO.spriteHeigth /
-                    asteroidSprite.ValueRO.pixelsPerUnit /
-                    asteroidSprite.ValueRO.spriteDivision *
-                    asteroid.ValueRO.sizeMultiplier;
-
-                float distanceX = distanceBetween.x -
-                    bulletHalfWidth - asteroidHalfWidth;
-
-                float distanceY = distanceBetween.y -
-                    bulletHalfHeigth - asteroidHalfHeight;
-
-
-                if (distanceX <= 0 || distanceY <= 0) {
-                    ecb.DestroyEntity(asteroidEntity);
-                    ecb.DestroyEntity(bulletEntity);
-                }
-            }
+            new BulletCollisionJob {
+                ecb = ecb,
+                asteroidTransform = asteroidTransform.ValueRO,
+                asteroidSprite = asteroidSprite.ValueRO,
+                asteroid = asteroid.ValueRO,
+                asteroidEntity = asteroidEntity
+            }.Schedule(state.Dependency).Complete();
         }
 
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+    }
+
+    [WithAny(typeof(Bullet))]
+    public partial struct BulletCollisionJob : IJobEntity {
+        public EntityCommandBuffer ecb;
+        public LocalTransform asteroidTransform;
+        public Texture asteroidSprite;
+        public Asteroid asteroid;
+        public Entity asteroidEntity;
+
+        void Execute(in Texture bulletSprite, in LocalTransform bulletTransform, 
+            in Entity bulletEntity) {
+            
+            float3 distanceBetween = math.distancesq(bulletTransform.Position,
+                asteroidTransform.Position);
+
+            float bulletHalfWidth = bulletSprite.spriteWidth / bulletSprite.pixelsPerUnit /
+                bulletSprite.spriteDivision;
+
+            float bulletHalfHeigth = bulletSprite.spriteHeigth / bulletSprite.pixelsPerUnit /
+                bulletSprite.spriteDivision;
+
+            float asteroidHalfWidth = asteroidSprite.spriteWidth / asteroidSprite.pixelsPerUnit /
+                asteroidSprite.spriteDivision * asteroid.sizeMultiplier;
+
+            float asteroidHalfHeight = asteroidSprite.spriteHeigth / asteroidSprite.pixelsPerUnit /
+                asteroidSprite.spriteDivision * asteroid.sizeMultiplier;
+
+            float distanceX = distanceBetween.x - bulletHalfWidth - asteroidHalfWidth;
+
+            float distanceY = distanceBetween.y - bulletHalfHeigth - asteroidHalfHeight;
+
+            if (distanceX <= 0 || distanceY <= 0) {
+                ecb.DestroyEntity(asteroidEntity);
+                ecb.DestroyEntity(bulletEntity);
+            }
+        }
     }
 }
